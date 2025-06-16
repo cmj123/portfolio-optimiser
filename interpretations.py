7499690aae071803a61885bc7cd4bd2da526c7e9
Esuabom Dijemeni <e.dijemeni@gmail.com>
2025-06-16 10:48:27 +0100

    Create app info on UI

---
+++ interpretations.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def appinfo():
+    st.markdown("This app optimizes a stock portfolio using a range of optimization techniques to identify the most efficient allocation based on the selected criteria. Simply enter your stock tickers, choose a historical date range, and watch as the app analyzes performance and delivers tailored allocation recommendations to help you achieve your investment goals.")
---
+++ main.py
@@ -0,0 +1,28 @@
+import datetime as dt
+import pandas as pd
+import streamlit as st
+import plotly.express as px 
+import streamlit_shadcn_ui as ui
+from PIL import Image
+from interpretations import appinfo
+
+
+def main():
+    im = Image.open("EfficientFrontier.png")
+
+    st.set_page_config(page_title="Portfolio Optimisation Dashboard", page_icon=im)
+
+    st.markdown("## Portfolio Optimisation Dashboard")
+    col1, col2 = st.columns([0.14, 0.86], gap="small")
+    col1.write("`Created by: `")
+    linkedin_url = "https://www.linkedin.com/in/edijemeni/"
+    col2.markdown(
+        f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="15" height="15" style="vertical-align: middle; margin-right: 10px;">`Esuabom Dijemeni`</a>',
+        unsafe_allow_html=True,
+    )
+
+    appinfo()
+
+
+if __name__ == "__main__":
+    main()
