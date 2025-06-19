7c9458277f8758c3d048fd6b4d6d38300f5fadde
Esuabom Dijemeni <e.dijemeni@gmail.com>
2025-06-19 09:45:13 +0100

    Complete getData and basicMetric function for portfolio optimiser

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import streamlit as st
 import plotly.express as px 
 import streamlit_shadcn_ui as ui
 from PIL import Image
+import time
 from interpretations import appinfo, optimization_strategies_info
+from portfolio_optimiser import PortfolioOptimiser
 
 
 def main():
@@ -76,6 +78,83 @@ def main():
 
     calc = cont1.button("Calculate")
     riskFreeRate = riskFreeRate_d/100
+
+    st.session_state.stocks_list = [s.strip() for s in stocks.split(",")]
+   
+
+    if calc:
+        try:
+            with st.spinner("Buckle Up! Financial Wizardy in Progress..."):
+                time.sleep(1)
+                stocks_list = st.session_state.stocks_list
+                optimiser = PortfolioOptimiser(
+                    stocks_list,
+                    start_date,
+                    end_date,
+                    optimisation_criterion,
+                    riskFreeRate
+                )
+        except ValueError as e:
+            st.error("Unable to download data for one or more tickers")
+            return
+        except Exception as e:
+            st.error(str(e))
+            return
+        
+        with st.container(border=True):
+            #  tab3, tab4, tab5
+            tab1, tab2 = st.tabs( 
+                [
+                    "Summary",
+                    "Efficient Frontier",
+                    # "Metrics",
+                    # "Portfolio Returns",
+                    # "Risk Analysis"
+                ]             
+            )
+            with tab1:
+                st.markdown('#### Optimised Porfolio Performance')
+                col1, col2 = st.columns(2)
+                col1.markdown(f"**Returns**: {46.41}%")
+                col1.markdown(f"**Volatility**: {22.75}%")
+                col1.markdown(f"**Sharpe Ratio**: {1.74}")
+                col1.markdown(f"**Sortino Ratio**: {2.91}")
+                col2.markdown(f"**Time Period**: {365} days")
+
+                st.markdown("#### Optimiserd Porfolio Allocation")
+                alocCol, pieCol = st.columns(2)
+                with alocCol:
+                    allocations = {
+                        'AAPL':48.72,
+                        "TSLA": 30.48,
+                        "MSFT":0,
+                        "SNOW":20.80
+                    }
+                    df_allocations = pd.DataFrame(allocations.items(), columns=['Tickers', 'Allocation (%)'])
+                    ui.table(df_allocations)
+                with pieCol:
+                    fig = px.pie(df_allocations, values='Allocation (%)',names='Tickers')
+                    fig.update_layout(
+                        width=180,
+                        height=200,
+                        # showlegend=False,
+                        margin=dict(t=20, b=0, l=0, r=0),
+                    )
+                    st.plotly_chart(fig, use_container_width=True)
+
+            # with tab2:
+            #         st.markdown("### Efficent Froniter Assets")
+                
+            # with tab3:
+            #     st.markdown("#### Risk and Return Metrics")
+            #     # ui.table()
+
+                
+                    
+
+
+
+
 
 if __name__ == "__main__":
     main()
---
+++ portfolio_optimiser.py
@@ -0,0 +1,54 @@
+import yfinance as yf 
+import numpy as np 
+import scipy.optimize as sc
+import pandas as pd
+import streamlit as st 
+import matplotlib.pyplot as plt
+import plotly.express as px 
+from scipy.stats import norm 
+
+class PortfolioOptimiser:
+
+    def __init__(
+            self, stocks, start, end, optimisation_criterion, riskFreeRate=0.045):
+        self.stocks = [stock for stock in stocks]
+        self.start = start
+        self.end = end
+        self.optimisation_criterion = optimisation_criterion
+        self.riskFreeRate =  riskFreeRate
+        self.meanReturns, self.covMatrix = self.getData()
+        # self.benchmark = self.benchmarkReturns()
+        # (
+        #     self.optimized_returns,
+        #     self.optimized_std,
+        #     self.optimized_allocation,
+        #     self.efficientList,
+        #     self.targetReturns,
+        # ) = self.calculatedResults()
+    
+    def basicMetrics(self):
+        if not all(s.isupper() for s in self.stocks):
+            raise ValueError("Enter ticker names in Capital Letters!")
+        if len(self.stocks) <=1:
+            raise ValueError("More than 1 ticker input required!")
+        try:
+            stockData = yf.download(self.stocks, start=self.start, end=self.end)
+        except:
+            raise ValueError("Unable to download data, try again later!")
+        stockData = stockData["Close"]
+
+        if len(stockData.columns) != len(self.stocks):
+            raise ValueError("Unable to download data for one or more tickers")
+        
+        returns = stockData.pct_change()
+        stdIndividual = returns.std()
+      
+        return returns, stdIndividual
+
+    def getData(self):
+
+        returns, stdIndividual = self.basicMetrics()
+        meanReturns = (returns.mean())
+        covMatrix = (returns.cov())
+
+        return meanReturns, covMatrix
